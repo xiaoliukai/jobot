4790312a484badd6d9d2823d25c8a9741dcb3339
Manuel Darveau <manueldarveau@gmail.com>
2013-10-27 22:12:37 -0400

    Added start script and tested with redis and openfire.

---
+++ run_localhost.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+export HUBOT_XMPP_USERNAME=hubot@manuel-darveaus-imac.local
+export HUBOT_XMPP_PASSWORD=hubot
+export HUBOT_XMPP_ROOMS=deploy@conference.manuel-darveaus-imac.local
+export HUBOT_XMPP_HOST=localhost
+export HUBOT_XMPP_PORT=5222
+
+./bin/hubot -a xmpp
