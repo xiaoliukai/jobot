8b55c26ace1cb04f513de840b5551f8437d3bef9
Manuel Darveau <manueldarveau@gmail.com>
2014-02-21 23:01:00 -0500

    Prepare for deployment @ 8D

--- run_8D.sh
+++ run_8D.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+export PATH="node_modules/.bin:node_modules/hubot/node_modules/.bin:$PATH"
+
 #export HUBOT_LOG_LEVEL=debug
+
+export FILE_BRAIN_PATH="."
+export HUDSON_TEST_MANAGER_URL='https://solic1.dev.8d.com:8443'
 
 # Set path to adapter since we are using npm link for hubot dependency. See Readme
 export HUBOT_ADAPTER_PATH=`pwd`/node_modules/
