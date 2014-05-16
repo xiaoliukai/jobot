22a2ea4dd8ddd186ff694081997dbdb9a8d0bcf8
Samuel Boucher <sboucher@8d.com>
2014-05-16 15:46:43 -0400

    clean the startup script

--- run_localhost.sh
+++ run_localhost.sh
@@ -9,10 +9,6 @@ export TEAMCITY_TEST_MANAGER_URL="https://teamcity.priv.8d.com:8443"
 # Set path to adapter since we are using npm link for hubot dependency. See Readme
 export HUBOT_ADAPTER_PATH=`pwd`/node_modules/
 export FILE_BRAIN_PATH=.
-#export HUBOT_TEAMCITY_USERNAME='jobot'
-#export HUBOT_TEAMCITY_PASSWORD='jobot'
-export HUBOT_TEAMCITY_HOSTNAME='teamcity.priv.8d.com:8443'
-export HUBOT_TEAMCITY_SCHEME='https'
 export HUBOT_XMPP_USERNAME=hubot@${HOSTNAME}
 export HUBOT_XMPP_CONFERENCE_DOMAINS=conference.${HOSTNAME}
 export HUBOT_XMPP_PASSWORD=hubot
