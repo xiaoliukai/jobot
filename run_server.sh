6250224929f6a5c77fd75ab21381733cbe5dc53a
Samuel Boucher <sboucher@8d.com>
2014-06-10 12:19:57 -0400

    i18n debug

--- run_server.sh
+++ run_server.sh
@@ -3,7 +3,7 @@
 #export HUBOT_LOG_LEVEL=debug
 
 export JAVA_HOME=/usr/local/jvm/latest7
-export M2_HOME=/data/cloud/apache-maven-3.0.3
+export M2_HOME=/data/cloud/apache-maven-jobot
 export PATH="$rootj_jobot/node_modules/.bin:$rootj_jobot/node_modules/hubot/node_modules/.bin:$PATH"
 export PATH=$PATH:$M2_HOME/bin:$JAVA_HOME:/usr/local/bin
 
@@ -11,11 +11,10 @@ export rootj=/data/cloud/jobot
 export rootj_jobot=$rootj/jobot
 export FILE_BRAIN_PATH=$rootj/data
 export I18N_WATCH_WORKDIR=$rootj/data/i18nwatch
+export JOBOT_LOG=$rootj/log
 
 #CI settings: URL of the service and select the service to be watched.
 export HUDSON=false
-export HUDSON_TEST_MANAGER_URL='https://hudson.priv.8d.com:8443'
-export TEAMCITY_TEST_MANAGER_URL="https://teamcity.priv.8d.com:8443"
 
 # Set path to adapter since we are using npm link for hubot dependency. See Readme
 export HUBOT_ADAPTER_PATH=$rootj_jobot/node_modules/
@@ -27,15 +26,10 @@ export HUDSON_TEST_MANAGER_DEFAULT_FIX_THRESHOLD_WARNING_HOURS=96       #96
 
 # export HUBOT_XMPP_PASSWORD=XXXXXXXXXXXXXXX
 # export JABBER_DOMAIN=XXXXXXXXXXXXX
+# export HUDSON_TEST_MANAGER_URL='https://url:port'
+# export TEAMCITY_TEST_MANAGER_URL="https://url:port"
+
 . $rootj/data/config
-
-
-# ${HUBOT_XMPP_PASSWORD:?"Need to set HUBOT_XMPP_PASSWORD: export HUBOT_XMPP_PASSWORD=..."}
-# if [ -z "$HUBOT_XMPP_PASSWORD" ]; then
-#     read -p "Jabber password:" HUBOT_XMPP_PASSWORD
-#     export HUBOT_XMPP_PASSWORD=$HUBOT_XMPP_PASSWORD
-#     echo $HUBOT_XMPP_PASSWORD
-# fi
 
 # Jabber connection :
 
@@ -48,4 +42,4 @@ export HUBOT_XMPP_PORT=5222
 
 cd  $rootj_jobot
 
-exec bin/hubot -n jobot -a xmpp > $rootj/data/jobot.log 2>&1
+exec bin/hubot -n jobot -a xmpp >> $JOBOT_LOG/jobot.log 2>&1
